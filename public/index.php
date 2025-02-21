7e96a9fc5612dbf80c6efa06f39cd67218cad9d2
IvanPedroSG <ivanpedrosampaio@gmail.com>
2025-02-21 20:00:09 -0300

    Estrutura do projeto modificada e início do front-end

--- public/index.php
+++ public/index.php
@@ -1,2 +1,26 @@
-// Arquivo de Entrada
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Plataforma completa para freelancers e autônomos, unindo o gerenciamento do tempo e a administração do dinheiro em um só lugar.">
+        <link rel="stylesheet" href="./assets/css/output.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="./assets/css/pages/index/index.css">
+        <title>Freela Flow Manager</title>
+    </head>
 
+    <body>
+        <header>
+            <img src="./assets/media/global/Freela_Flow_Manager_minimalist_removed_bg.png" alt="FFM Icon"><h1>FFM</h1>
+        </header>
+        
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
