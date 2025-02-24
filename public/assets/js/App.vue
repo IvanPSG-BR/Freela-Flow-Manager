4bf3e7acf627a37442f2a51ece9cdc4bef420555
IvanPedroSG <ivanpedrosampaio@gmail.com>
2025-02-24 00:12:30 -0300

    Correção "Tudo bem, eu não preciso monitorar meu site, só preciso que ele FUNCIONE"

--- public/assets/js/App.vue
+++ public/assets/js/App.vue
@@ -3,14 +3,9 @@
 </template>
 
 <script>
-  import { SpeedInsights } from "@vercel/speed-insights/vue"
-  import { Analytics } from "@vercel/analytics/react"
   export default {
     name: 'App', // Nome do componente
   };
-
-  SpeedInsights();
-  Analytics();
 </script>
 
 <style>
