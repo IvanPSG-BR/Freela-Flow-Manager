28dbb6308a98a91181bf62b505432df4f068a637
IvanPSG-BR <ivanpedrosampaio@gmail.com>
2025-03-08 22:16:46 -0300

    Adicionada responsividade para diferentes tamanhos de tela

--- src/views/home/home.php
+++ src/views/home/home.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X0ST7CV8KR"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X0ST7CV8KR');
+        </script>
+
         <!-- Metadados da página -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,35 +37,32 @@
         <!-- Cabeçalho da página -->
         <header class="flex justify-between">
 
-            <div class="logo flex place-items-center transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-1.5">
+            <div class="logo flex place-items-center p-1 transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-2">
                 <!-- Link para a página inicial (TROCAR href AO EXECUTAR auto_upl.py) -->
                 <a href="/src/views/home/home.php"> <!-- Live Share: "/src/views/home/home.php" - Website: "/") -->
-
-                    <h1 class="website_name p-1 inline align-middle">
+                    <h1 class="website_name text-2xl md:text-3xl lg:text-4xl inline align-middle">
                         Freela 
-                        <img src="public/media/global/favicon2.ico" alt="FFM Icon" class="website_icon w-13 h-auto p-1 inline"> 
+                        <img src="public/media/global/favicon2.ico" alt="FFM Icon" class="website_icon w-10 md:w-12 lg:w-14 h-auto inline"> 
                         Manager
                     </h1>
-
                 </a>
-
             </div>
 
             <nav class="navbar flex place-items-center">
 
-                <a href="/?page=placeholder"><h2 class="page1 px-3 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-y-1.5">Opção 1</h2></a>
+                <a href="/?page=placeholder" class="page1 text-xs md:text-sm lg:text-base px-3 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-y-1.5">Opção 1</a>
 
-                <a href="/?page=placeholder"><h2 class="page2 px-3 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-y-1.5">Opção 2</h2></a>
+                <a href="/?page=placeholder" class="page2 text-xs md:text-sm lg:text-base px-3 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-y-1.5">Opção 2</a>
 
-                <a href="/?page=placeholder"><h2 class="page3 px-3 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-y-1.5">Opção 3</h2></a>
+                <a href="/?page=placeholder" class="page3 text-xs md:text-sm lg:text-base px-3 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:translate-y-1.5">Opção 3</a>
 
             </nav>
 
             <div class="auth_options flex place-items-center">
 
-                <a href="/?page=placeholder" class="signin_btn px-2 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:-translate-x-1.5 hover:translate-y-1.5">Cadastrar</a>
+                <a href="/?page=placeholder" class="signin_btn text-xs md:text-sm lg:text-base px-2 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:-translate-x-1.5 hover:translate-y-1.5">Cadastrar</a>
 
-                <a href="/?page=placeholder" class="login_btn px-2 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:-translate-x-1.5 hover:translate-y-1.5">Entrar</a>
+                <a href="/?page=placeholder" class="login_btn text-xs md:text-sm lg:text-base px-2 flex transition delay-100 duration-300 ease-initial hover:scale-110 hover:-translate-x-1.5 hover:translate-y-1.5">Entrar</a>
 
             </div>
 
--- src/views/placeholder/placeholder.php
+++ src/views/placeholder/placeholder.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Olá Mundo!</h1>
-    <h2>Essa é uma página que aparece quando o que você queria acessar não está pronto.</h2>
-    <a href="/">Página Inicial</a>
-</body>
+    <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X0ST7CV8KR"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X0ST7CV8KR');
+        </script>
+            
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FFM - Placeholder</title>
+    </head>
+
+    <body>
+        <h1>Olá Mundo!</h1>
+        <h2>Essa é uma página que aparece quando o que você queria acessar não está pronto.</h2>
+        <a href="/">Página Inicial</a>
+    </body>
 </html>
